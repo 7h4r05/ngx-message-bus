3115256c9b755dbe516bdfb61d441cf135986030
Dariusz Zabrzeński <dariusz.zabrzenski@gmail.com>
2017-11-07 23:23:05 +0100

    Remove unnecessary header files

--- src/message.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Message<T> {
-    payload: T;
-    groupId: string;
-    recipentIds: string[];
-    publisherId?: string;
-    timeGenerated?: Date;
-    metadata?: string;
-}
--- src/subscription.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class Subscription<T> {
-    groupId: string;
-    callback: (payload: T) => void;
-}
